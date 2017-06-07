77d3849f815e3d24c742e6a6bf496b4bbfc36e14
ddandipu <andiputra.dimas@hotmail.com>
2017-06-07 00:47:02 +0000

    completed exercise 6

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,11 +6,10 @@ puts "----------"
 # Your code goes below here ...
 
 class Store < ActiveRecord::Base
-
+  has_many :employees
 end
 
 Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
-puts Store.count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+
+Employee.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60, store_id: 1)
+Employee.create(first_name: "Dimas", last_name: "Andiputra", hourly_rate: 20, store_id: 1)
+Employee.create(first_name: "Brad", last_name: "Pitt", hourly_rate: 100, store_id: 2)
+Employee.create(first_name: "Angelina", last_name: "Jolie", hourly_rate: 40, store_id: 2)
